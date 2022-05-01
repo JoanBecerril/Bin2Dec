7461487692d5f685b534ec4b07119b4c75c821fb
Joan Becerril Hermida <100007464.joan23@fje.edu>
2022-05-01 21:13:39 +0200

    subida 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-function parrafos() {
-    var infonum = document.getElementById('infonum')
-    infonum.innerHTML = 'You entered a non binary digit (please enter only 0 or 1)'
-    var infonum2 = document.getElementById('infonum2')
-    if (numero << 1) {
-        var infonum2 = 'Here is your decimal'
+function JoanBin2Dec() {
+    var infobien = document.getElementById('infobien')
+    var infoerror = document.getElementById('infoerror')
+    var resultadobinario = document.getElementById('resultadobinario').value
+    var procesando = document.getElementById('procesando')
+    procesando.innerHTML = parseInt(resultadobinario, 2);
+    var numero = resultadobinario.charAt(resultadobinario.length - 1)
+    if (resultadobinario == '') {
+        procesando.innerHTML = ''
+    }
+    if (numero == 0 || numero == 1) {
+        infobien.innerHTML = 'Here is your decimal!'
+        infoerror.innerHTML = ''
+    } else {
+        procesando.innerHTML = '🤷🏻‍♀️Waiting for a valid binary number...'
+    }
+    if (numero >> 1 || numero >> 10000) {
+        infoerror.innerHTML = 'You entered a non-binary digit (please enter only 0 or 1)'
+        infobien.innerHTML = ''
+    }
+    if (numero == '') {
+        infoerror.innerHTML = ''
+        infobien.innerHTML = ''
     }
 }
+
+
+
+// 🤷🏻‍♀️Waiting for a valid binary number... //
+// You entered a non-binary digit (please enter only 0 or 1) //
+// Here is your decimal! //
