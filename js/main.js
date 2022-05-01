af4f3249e9240c994abe6b2c814a77d718a34dff
Joan Becerril Hermida <100007464.joan23@fje.edu>
2022-05-01 21:18:46 +0200

    subida 5

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ function JoanBin2Dec() {
     var infobien = document.getElementById('infobien')
     var infoerror = document.getElementById('infoerror')
     var resultadobinario = document.getElementById('resultadobinario').value
-    var procesando = document.getElementById('procesando')
-    procesando.innerHTML = parseInt(resultadobinario, 2);
+    var result = document.getElementById('result')
+    result.innerHTML = parseInt(resultadobinario, 2);
     var numero = resultadobinario.charAt(resultadobinario.length - 1)
     if (resultadobinario == '') {
-        procesando.innerHTML = ''
+        result.innerHTML = ''
     }
     if (numero == 0 || numero == 1) {
         infobien.innerHTML = 'Here is your decimal!'
         infoerror.innerHTML = ''
     } else {
-        procesando.innerHTML = '🤷🏻‍♀️Waiting for a valid binary number...'
+        result.innerHTML = '🤷🏻‍♀️Waiting for a valid binary number...'
     }
     if (numero >> 1 || numero >> 10000) {
         infoerror.innerHTML = 'You entered a non-binary digit (please enter only 0 or 1)'
